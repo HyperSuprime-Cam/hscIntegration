5c7da898e00d009a218b55fd3fb7a175aa54d407
Paul Price <price@astro.princeton.edu>
2012-06-26 18:14:50 -0400

    Add test of reduceFrames.py

--- bin/hscIntegration.py
+++ bin/hscIntegration.py
@@ -3,6 +3,7 @@
 import os.path
 from hsc.integration.integration import Integrator
 from hsc.integration.processCcd import ProcessCcdTest
+from hsc.integration.reduceFrames import ReduceFramesTest
 from hsc.integration.data import DataTest, CalibTest
 
 integrator = Integrator(tests=[
@@ -10,12 +11,14 @@ integrator = Integrator(tests=[
              'DATA'),
     CalibTest("scCalib", "suprimecam", os.path.join(os.environ['HSCINTEGRATIONDATA_DIR'], 'SC-Calib'), 
               'DATA/SUPA/CALIB', validity=90),
-    ProcessCcdTest("SUPA01087235", "suprimecam", 108723, 5, dir='DATA/SUPA', rerun="test"),
+    ProcessCcdTest("SUPA01087235", "suprimecam", 108723, 5, dir='DATA/SUPA', rerun="processCcd"),
 
     DataTest("hscData", "hscsim", os.path.join(os.environ['HSCINTEGRATIONDATA_DIR'], 'HSC'), 'DATA'),
     CalibTest("hscCalib", "hscsim", os.path.join(os.environ['HSCINTEGRATIONDATA_DIR'], 'HSC-Calib'),
               'DATA/HSC/CALIB'),
-    ProcessCcdTest("HSCA00243100", "hscsim", 243, 100, dir='DATA/HSC', rerun="test"),
+    ProcessCcdTest("HSCA00243100", "hscsim", 243, 100, dir='DATA/HSC', rerun="processCcd"),
+
+    ReduceFramesTest("SUPA0108723X", "suprimecam", [108723], dir='DATA/SUPA', rerun="reduceFrames")
     ])
 
 
