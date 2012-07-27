031176ec48961ea3774efc4613ccc23dcaecc43a
Paul Price <price@astro.princeton.edu>
2012-07-27 07:16:27 +0000

    Convert to using an abstract base class, and selecting via a factory function.

--- python/hsc/integration/camera.py
+++ python/hsc/integration/camera.py
@@ -1,14 +1,49 @@
 import os.path
 
 class CameraInfo(object):
-    def __init__(self, camera):
-        if camera.lower() in ("suprimecam", "suprime-cam", "sc"):
-            self.addDir = "SUPA"
-            self.refileScript = "refileSupaFiles.py"
-            self.override = os.path.join(os.environ['HSCPIPE_DIR'], 'config', 'suprimecam.py')
-        elif camera.lower() in ("hsc", "hscsim"):
-            self.addDir = "HSC"
-            self.refileScript = "refileHSCFiles.py"
-            self.override = os.path.join(os.environ['HSCPIPE_DIR'], 'config', 'hsc.py')
-        else:
-            raise RuntimeError("Unrecognised camera: %s" % camera)
+    """Base class for camera-specific options"""
+    def __init__(self, **kwargs):
+        for kw in ("addDir",            # Additional directory elements to add to workdir
+                   "refileScript",      # Name of script for filing exposures
+                   "override",          # Name of configuration override file for processCcd
+                   "dbRaw",             # Name of script for registering raw exposures in PGSQL
+                   "dbFrame",           # Name of script for registering processed frames in PGSQL
+                   "dbExposure",        # Name of script for registering processed exposure in PGSQL
+                   "inputCat",          # Name of instrument in inputCat script
+                   ):
+            assert(kw in kwargs)
+            setattr(self, kw, kwargs[kw])
+
+
+class SuprimeCamCameraInfo(CameraInfo):
+    def __init__(self):
+        super(SuprimeCamCameraInfo).__init__(
+            addDir = "SUPA",
+            refileScript = "refileSupaFiles.py",
+            override = os.path.join(os.environ['HSCPIPE_DIR'], 'config', 'suprimecam.py')
+            dbRaw = "regist_raw_Suprime.py",
+            dbFrame = "frame_regist_CorrSuprime.py",
+            dbExposure = "exposure_regist_CorrSuprime.py",
+            inputCat = "SUP",
+            )
+
+class HscCameraInfo(CameraInfo):
+    def __init__(self):
+        super(SuprimeCamCameraInfo).__init__(
+            addDir = "HSC",
+            refileScript = "refileHscFiles.py",
+            override = os.path.join(os.environ['HSCPIPE_DIR'], 'config', 'hsc.py')
+            dbRaw = "regist_raw_Hsc.py",
+            dbFrame = "frame_regist_CorrHsc.py",
+            dbExposure = "exposure_regist_CorrHsc.py",
+            inputCat = "HSC",
+            )
+
+
+def getCameraInfo(camera):
+    """Factory function to produce desired CameraInfo derived class"""
+    if camera.lower() == "suprimecam":
+        return SuprimeCamCameraInfo()
+    if camera.lower() == "hsc":
+        return HscCameraInfo()
+    raise RuntimeError("Unrecognised camera name: %s" % camera)
