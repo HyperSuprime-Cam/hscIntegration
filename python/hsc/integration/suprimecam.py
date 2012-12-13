33126e5ceb18a4c57dd1a0830690445c65c60a76
Paul Price <price@astro.princeton.edu>
2012-12-13 12:16:56 -0500

    Update Suprime-Cam tests.

--- python/hsc/integration/suprimecam.py
+++ python/hsc/integration/suprimecam.py
@@ -22,14 +22,18 @@ hscPipeDir = os.environ['HSCPIPE_DIR']
 
 
 Integration.register(DataTest("scData", "suprimecam", os.path.join(dataDir, 'SuprimeCam')))
-Integration.register(ReduceDetrendsTest("scDetrend", "suprimecam", "flat", range(108390, 108401),
-                                        rerun="detrend"))
-Integration.register(CalibTest("scCalib", "suprimecam", validity=90))
+#Integration.register(ReduceDetrendsTest("scBias", "suprimecam", "bias", {'field': 'BIAS'},
+#                                        {'calibVersion': 0}, rerun="bias"))
+#Integration.register(CalibTest("scCalibBias", "suprimecam", validity=90))
+Integration.register(ReduceDetrendsTest("scFlat", "suprimecam", "flat", {'field': 'DOMEFLAT'},
+                                        {'calibVersion': "one"}, rerun="flat"))
+Integration.register(CalibTest("scCalibFlat", "suprimecam", validity=90))
 Integration.register(ProcessCcdTest("SUPA01087235", "suprimecam", 108723, 5, rerun="processCcd"))
 Integration.register(ProcessCcdTest("onsite", "suprimecam", 108723, 0, rerun="onsite",
                                     addOptions="-C " + os.path.join(hscPipeDir, 'config', 'onsite.py')))
-Integration.register(ReduceFramesTest("reduceFrames-SC", "suprimecam", [108723, 108724, 108725],
-                                      rerun="reduceFrames", time=4000))
+Integration.register(ReduceFramesTest("reduceFrames-SC", "suprimecam", [{'visit': '108723..108725'}],
+                                      [{'visit': v, 'ccd': c} for v in (108723, 108724, 108725)
+                                       for c in range(10)], rerun="reduceFrames", time=4000))
 Integration.register(SolveTansipTest("solvetansip", "suprimecam", 108723, rerun="reduceFrames"))
 Integration.register(ReduceStackTest("reduceStack-SC", "suprimecam", "SDSS1115", "W-S-I+",
                                      rerun="reduceFrames"))
