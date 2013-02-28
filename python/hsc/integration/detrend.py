1ac360cb6c84f71f70d4ab16f5064bac75dacbe9
Paul Price <price@astro.princeton.edu>
2013-02-28 23:52:14 +0900

    Constructed calibs now end up in the rerun CALIB directory, rather than the root CALIB directory.  This is probably an improvement, but I need to handle the change.  For now, just put in links to the right place.

--- python/hsc/integration/detrend.py
+++ python/hsc/integration/detrend.py
@@ -7,7 +7,7 @@ import lsst.afw.image.utils as afwIU
 
 
 class ReduceDetrendsTest(PbsTest, CcdValidationTest):
-    def __init__(self, name, camera, detrend, idDict, detrendIdDict, time=1200, rerun=None, **kwargs):
+    def __init__(self, name, camera, detrend, idDict, detrendIdDict, rerun, time=1200, **kwargs):
         self.camera = camera
         self.detrend = detrend
         self.rerun = rerun
@@ -23,16 +23,23 @@ class ReduceDetrendsTest(PbsTest, CcdValidationTest):
         cameraInfo = getCameraInfo(self.camera)
 
         command = os.path.join(os.environ['HSCPIPE_DIR'], 'bin', exeNames[detrend]) + " "
-        command += " " + camera + " @WORKDIR@/" + cameraInfo.addDir + " "
+        command += " @WORKDIR@/" + cameraInfo.addDir + " "
         command += " --id " + " ".join("=".join(map(str,kv)) for kv in idDict.items())
         command += " --detrendId " + " ".join("=".join(map(str,kv)) for kv in detrendIdDict.items())
         command += " --job=" + name
         command += " --time=%f" % time
-        if rerun is not None:
-            command += " --rerun=" + rerun
         command += " @PBSARGS@"
+        command += " --rerun=" + rerun
 
-        super(ReduceDetrendsTest, self).__init__(name, ["pbs", "calib", detrend, camera], [command], **kwargs)
+        # XXX The calibs used to end up in the root CALIB directory, but since Jim changed the rerun handling,
+        # they go in a new rerun CALIB directory.  I'm more concerned to get things working than fix things up,
+        # so here's a workaround.
+        create = "mkdir -p @WORKDIR@/" + cameraInfo.addDir + "/CALIB"
+        link = "ln -s ../rerun/" + detrend + "/CALIB/" + detrend.upper()
+        link += " @WORKDIR@/" + cameraInfo.addDir + "/CALIB/" + detrend.upper()
+
+        super(ReduceDetrendsTest, self).__init__(name, ["pbs", "calib", detrend, camera],
+                                                 [command, create, link], **kwargs)
 
 #    def preHook(self, workDir=".", **kwargs):
 #        suprimeDataDir = os.path.split(os.path.abspath(workDir))
