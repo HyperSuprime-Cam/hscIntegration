c0ea2419a0f1171872d1077dac6c9a477664bc3f
Paul Price <price@astro.princeton.edu>
2013-12-11 09:07:38 +0900

    hsc: use comprehensive set of tests for HSC, like Suprime-Cam

--- python/hsc/integration/hscTests.py
+++ python/hsc/integration/hscTests.py
@@ -9,17 +9,27 @@ from hsc.integration.solvetansip import SolveTansipTest
 from hsc.integration.pipeQa import PipeQaTest
 
 dataDir = os.environ['HSCINTEGRATIONDATA_DIR']
-hscPipeDir = os.environ['HSCPIPE_DIR']
 
-readDataIdList = [{'visit': v, 'ccd': c} for v in (902042,) for c in range(112)]
-processDataIdList = [dataId for dataId in readDataIdList if dataId['ccd'] < 104]
 
-Integration.register(DataTest("hscData", "hsc", os.path.join(dataDir, 'HSC'), readDataIdList))
-Integration.register(CalibTest("hscCalibFlat", "hsc", "flat", readDataIdList,
-                               source=os.path.join(dataDir, 'HSC-Calib')))
-Integration.register(ProcessCcdTest("HSCA902042049", "hsc", 902042, 49, rerun="processCcd"))
-Integration.register(ProcessCcdTest("HSCA902042103", "hsc", 902042, 103, rerun="processCcd")) # rotated
-Integration.register(ProcessCcdTest("HSCA902042111", "hsc", 902042, 111, rerun="processCcd")) # focus
-Integration.register(ReduceFramesTest("reduceFrames-HSC", "hsc", [{'visit': '902042'}],
-                                      processDataIdList, rerun="reduceFrames", time=100000))
-Integration.register(SolveTansipTest("solvetansip-HSC", "hsc", 902042, rerun="reduceFrames"))
+# This only includes a single visit to save time, but it does cover the focus and guide CCDs.
+singleFrame = [{'visit': 902876, 'ccd': c} for c in range(112)]
+# This only covers 104 CCDs because we explicitly drop all by the science CCDs in reduceFrames.
+multiFrames = [{'visit': v, 'ccd': c} for v in range(902872, 902890, 2) for c in range(104)]
+
+Integration.register(DataTest("hscData", "hsc", os.path.join(dataDir, 'HSC'), singleFrame))
+Integration.register(ReduceDetrendsTest("hscBias", "hsc", "bias", {'field': 'BIAS'},
+                                        {'calibVersion': "0"}, rerun="bias"))
+Integration.register(CalibTest("hscCalibBias", "hsc", "bias", singleFrame, validity=90))
+Integration.register(ReduceDetrendsTest("hscFlat", "hsc", "flat", {'field': 'DOMEFLAT'},
+                                        {'calibVersion': "one"}, rerun="flat"))
+Integration.register(CalibTest("hscCalibFlat", "hsc", "flat", singleFrame, validity=90))
+Integration.register(ProcessCcdTest("HSC-902876-049", "hsc", 902876, 49, rerun="processCcd"))
+Integration.register(ProcessCcdTest("HSC-902876-054", "hsc", 902876, 54, rerun="processCcd"))
+Integration.register(ProcessCcdTest("HSC-902876-090", "hsc", 902876, 90, rerun="processCcd"))
+Integration.register(ProcessCcdTest("HSC-902876-103", "hsc", 902876, 103, rerun="processCcd"))
+Integration.register(ProcessCcdTest("HSC-902876-111", "hsc", 902876, 111, rerun="processCcd"))
+Integration.register(ReduceFramesTest("reduceFrames-HSC", "hsc", [{'visit': '902876..902880:2'}],
+                                      multiFrames, rerun="reduceFrames", time=300))
+Integration.register(SolveTansipTest("solvetansip-HSC", "hsc", 902876, rerun="reduceFrames"))
+Integration.register(ReduceStackTest("reduceStack-HSC", "hsc", "ABELL2163", "HSC-I",
+                                     rerun="reduceFrames"))
